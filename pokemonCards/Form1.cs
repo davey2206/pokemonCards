d48bc51bb18b6cf5a9391f9039f90f3aae913fa6
davey van den eijnde <davey2206@LIVE.NL>
2022-02-10 15:06:19 +0100

    images test

--- pokemonCards/Form1.cs
+++ pokemonCards/Form1.cs
@@ -7,7 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections;
 using PokemonTcgSdk;
+using System.Net;
 
 namespace pokemonCards
 {
@@ -16,12 +18,49 @@ namespace pokemonCards
         public Form1()
         {
             InitializeComponent();
+            var sets = Sets.All();
+            foreach (var set in sets)
+            {
+                comboBox1.Items.Add(set.Code.ToString());
+            }
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
         {
-            var card = Card.Find<Pokemon>("base1-4");
-            MessageBox.Show(card.Card.Name.ToString());
+            for (int i = 0; i < 10; i++)
+            {
+                foreach (Control control in this.panel1.Controls)
+                {
+                    PictureBox picture = control as PictureBox;
+                    this.panel1.Controls.Remove(picture);
+                }
+            }
+            int x = 0;
+            int y = 0;
+            Dictionary<string, string> query = new Dictionary<string, string>()
+            {
+                { "setCode", comboBox1.SelectedItem.ToString() }
+            };
+            var cards = Card.Get(query);
+            if (cards.Cards != null)
+            {
+                foreach (var card in cards.Cards)
+                {
+                    PictureBox pictureBox1 = new PictureBox();
+                    pictureBox1.Location = new System.Drawing.Point(x, y);
+                    pictureBox1.Name = "pictureBox1";
+                    pictureBox1.Size = new System.Drawing.Size(125, 175);
+                    pictureBox1.Load(card.ImageUrl);
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                    panel1.Controls.Add(pictureBox1);
+                    x = x + 130;
+                    if (x == 780)
+                    {
+                        x = 0;
+                        y = y + 180;
+                    }
+                }
+            }
         }
     }
 }
